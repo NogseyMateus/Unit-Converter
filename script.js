40f5a4f309f37a836fe21f86d1590f86fc2edda4
Mateus Nogueira <mfnogueira.nogueira@gmail.com>
2025-06-25 18:37:39 -0300

    Polishing the app.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.getElementById("type").addEventListener("change", function() {
+document.getElementById("type").addEventListener("change", function () {
   const unitSelct = document.getElementById("unit");
-  const type = this.value;
-  if (type === "temp"){
+  const type = this.value; //this is refering to "type"
+  if (type === "temp") {
     unitSelct.innerHTML = `   
       <option selected disabled>From/to</option>
       <option value="celsiusToFah">Celsius To Fahrenheit</option>
       <option value="fahToCelsius">Fahrenheit to Celsius</option>`;
     document.getElementById("unit").style.display = "block";
-  } else{
+  } else {
     unitSelct.innerHTML = `   
       <option selected disabled>From/to</option>
       <option value="kmToMil">Kilometers To Miles</option>
@@ -16,9 +16,9 @@ document.getElementById("type").addEventListener("change", function() {
   }
 });
 // Displaying the button after a unit has been chosen
-document.getElementById("unit").addEventListener("change", function(){
-const button = document.querySelector("button");
-button.style.display = "block";
+document.getElementById("unit").addEventListener("change", function () {
+  const button = document.querySelector("button");
+  button.style.display = "block";
 });
 
 function convert() {
@@ -29,18 +29,18 @@ function convert() {
   let resultString;
 
   if (unit === "milToKm") {
-    result = inputValue * 1.60934;
+    result = (inputValue * 1.60934).toFixed(2);
     resultString = `${inputValue}Miles is ${result}Km`;
   } else if (unit === "kmToMil") {
-    result = inputValue / 1.60934;
+    result = (inputValue / 1.60934).toFixed(2);
     resultString = `${inputValue}Km is ${result}Miles`;
   } else if (unit === "celsiusToFah") {
-    result = (inputValue * (9/5)) + 32;
+    result = (inputValue * (9 / 5) + 32).toFixed(2);
     resultString = `${inputValue}C° is ${result}F°`;
   } else if (unit === "fahToCelsius") {
-    result = (inputValue - 32) * (5/9);
+    result = ((inputValue - 32) * (5 / 9)).toFixed(2);
     resultString = `${inputValue}F° is ${result}C°`;
-  }
+  } //Add another operation Later
 
   resultElement.innerHTML = resultString;
-}  
+}
