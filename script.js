3b15bd1923f9d5545bc2221662426044497e18c4
Mateus Nogueira <mfnogueira.nogueira@gmail.com>
2025-06-25 14:44:20 -0300

    Adding New functionalities

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
+document.getElementById("type").addEventListener("change", function() {
+  const unitSelct = document.getElementById("unit");
+  const type = this.value;
+  if (type === "temp"){
+    unitSelct.innerHTML = `   
+      <option selected disabled>From/to</option>
+      <option value="celsiusToFah">Celsius To Fahrenheit</option>
+      <option value="fahToCelsius">Fahrenheit to Celsius</option>`;
+    document.getElementById("unit").style.display = "block";
+  } else{
+    unitSelct.innerHTML = `   
+      <option selected disabled>From/to</option>
+      <option value="kmToMil">Kilometers To Miles</option>
+      <option value="milToKm">Miles to Kilometers</option>`;
+    document.getElementById("unit").style.display = "block";
+  }
+});
+// Displaying the button after a unit has been chosen
+document.getElementById("unit").addEventListener("change", function(){
+const button = document.querySelector("button");
+button.style.display = "block";
+});
+
 function convert() {
-  const inputValue = document.getElementById("userInput").value; //getting the imput value
-  const unit = document.getElementById("unit").value; // getting the unit value
-  let milesToKm = unit === "milesToKm";
+  const inputValue = document.getElementById("userInput").value;
+  const unit = document.getElementById("unit").value;
   let result = 0;
   const resultElement = document.getElementById("resultElement");
+  let resultString;
 
-  if (milesToKm) {
-    result = inputValue * 1.60934; // conversion from miles to KM
-  } else {
-    result = inputValue / 1.60934; //Convesion from Km to miles
+  if (unit === "milToKm") {
+    result = inputValue * 1.60934;
+    resultString = `${inputValue}Miles is ${result}Km`;
+  } else if (unit === "kmToMil") {
+    result = inputValue / 1.60934;
+    resultString = `${inputValue}Km is ${result}Miles`;
+  } else if (unit === "celsiusToFah") {
+    result = (inputValue * (9/5)) + 32;
+    resultString = `${inputValue}C° is ${result}F°`;
+  } else if (unit === "fahToCelsius") {
+    result = (inputValue - 32) * (5/9);
+    resultString = `${inputValue}F° is ${result}C°`;
   }
 
-  const resultString = `${inputValue} miles are ${result} Km`;
   resultElement.innerHTML = resultString;
-  console.log(resultString);
 }  
